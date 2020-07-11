e396d969ed61a979e3b69c5153aad41fe3a8de44
ShadowMuffin <liordah0@gmail.com>
2020-07-11 14:13:31 +0300

    Create argparser

---
+++ src/argparser.py
@@ -0,0 +1,33 @@
+import argparse
+
+def get_desktop_path():
+    import os
+    return os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+
+class ArgParser():
+    
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description='A cool software to backup your file in the coolest way ever')
+        self.generate_arguments()
+        
+        
+    def generate_arguments(self):
+        self.parser.add_argument('path', type=str, help='Path of the file or directory to backup - Required')
+        self.parser.add_argument('dest', type=str, nargs='?', help='Optional destination of the backup, defaults to Desktop', default=get_desktop_path())
+        self.parser.add_argument('-d', '--directory', help = 'Whether the path is for a directory', action='store_true')
+        self.parser.add_argument('-v', '--version', action='version', version='BUCRS 1.0')
+    
+    def parse_arguments(self):
+        return self.parser.parse_args()
+
+    
+
+
+def main():
+    parser = ArgParser()
+    results = parser.parse_arguments()
+    print(results.dest)
+
+
+if __name__ == '__main__':
+    main()
